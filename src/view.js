d99684eadbf3edf50f381ebedc888600da928a0f
Alex Jongman <alex.jongman@hu.nl>
2020-05-09 23:29:56 +0200

    first commit

---
+++ src/view.js
@@ -0,0 +1,11 @@
+function getText() {
+    return 'Action test';
+}
+
+function addText() {
+    const text_node = document.querySelector('.text');
+    const text = document.createTextNode(getText());
+    text_node.append(text);
+}
+
+addText();
