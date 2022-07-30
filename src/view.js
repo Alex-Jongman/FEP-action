833604ad3cb2194dbb941a7626022a9a2f913081
Alex Jongman <alex.jongman@hu.nl>
2022-07-30 11:55:38 +0200

    small change

--- src/view.js
+++ src/view.js
@@ -1,11 +1,9 @@
-export default function getText() {
+export function getText() {
   return 'Action test';
 }
 
-// function addText() {
-//   const TEXT_NODE = document.querySelector('.text');
-//   const text = document.createTextNode(getText());
-//   TEXT_NODE.append(text);
-// }
-
-// addText();
+export function addText() {
+  const TEXT_NODE = document.querySelector('.text');
+  const text = document.createTextNode(getText());
+  TEXT_NODE.append(text);
+}
